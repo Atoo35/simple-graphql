c4f1a1bec1f785eb12a9628e66c0b6fc24a31163
Atharva Deshpande <adrooney322@gmail.com>
2023-01-03 11:16:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ app.use('/graphql', expressGraphQl({
   schema,
   graphiql: true,
 }));
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('Server is running on port 3000');
 });
